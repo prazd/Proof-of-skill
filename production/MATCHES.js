26584122a9302903cd37254594c80e9d5f266188
Furazer <v.kolesnickoff2012@yandex.ru>
2018-05-20 01:35:06 +0300

    view db

---
+++ production/MATCHES.js
@@ -0,0 +1,58 @@
+var data = [{
+		"id" : 1,
+		"team1" : "RUS",
+		"team2" : "SA",
+		"coef1" : 1.35 ,
+		"coef2" : 12.75,
+		"coefX" : 4.95
+	},
+	
+	{
+		"id" : 2,
+		"team1" : "EG",
+		"team2" : "UR",
+		"coef1" : 5.50 ,
+		"coef2" : 1.75,
+		"coefX" : 3.6
+	},
+
+	{
+		"id" : 3,
+		"team1" : "MAR",
+		"team2" : "IR",
+		"coef1" : 2.35 ,
+		"coef2" : 3.05,
+		"coefX" : 3.65
+	},
+	{
+		"id" : 4,
+		"team1" : "POR",
+		"team2" : "SPA",
+		"coef1" : 4.6 ,
+		"coef2" : 3.5,
+		"coefX" : 1.9
+	},
+	{
+		"id" : 5,
+		"team1" : "FR",
+		"team2" : "AUS",
+		"coef1" : 1.22 ,
+		"coef2" : 6.25,
+		"coefX" : 19
+	},
+	{
+		"id" : 6,
+		"team1" : "ARG",
+		"team2" : "IS",
+		"coef1" : 1.35 ,
+		"coef2" : 4.8,
+		"coefX" : 12.25
+	},
+	{
+		"id" : 7,
+		"team1" : "PER",
+		"team2" : "DAN",
+		"coef1" : 3.85 ,
+		"coef2" : 3.40,
+		"coefX" : 2.1
+	}]
